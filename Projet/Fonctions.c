38a8b20783a6dc944a62800d13c1b1fd44bd3696
Mahkss <max.pavet@viacesi.fr>
2016-12-16 11:51:23 +0100

    Update Fonctions.c

--- Projet/Fonctions.c
+++ Projet/Fonctions.c
@@ -3,18 +3,18 @@
 #include <time.h>
 #include <unistd.h>
 
-int choix(int modulo){
+int choix(int modulo){	//fonction de génération aléatoire
 
- srand(time(NULL));
+ srand(time(NULL));       
  int ran = 0;
- return ran = rand() %modulo;
+ return ran = rand() %modulo;	//renvoie un int désignant l'élement choisi
  }
 
 
-void execTerm1(int pbm){
+void execTerm1(int pbm){	//Fonction d'execution du TermSaver statique selon l'image choisi par fonction "choix"
 switch (pbm){
   case 0:
-    putenv("MONPBM=ex1.pbm");
+    putenv("MONPBM=ex1.pbm");	//MONPBM est une variable d'environempent désignant le fichier de l'image
     break;
   case 1:
     putenv("MONPBM=ex2.pbm");
@@ -29,16 +29,16 @@ switch (pbm){
     putenv("MONPBM=ex5.pbm");
     break;
   default:
-    printf("probleme de choix de l'image");
+    printf("probleme de choix de l'image");	//Si aucune image n'a été choisi (improbable)
     break;
   }
 
-  chdir(getenv("EXIASAVER_HOME"));
-  system("./T1");
+  chdir(getenv("EXIASAVER_HOME"));	//On se place dans le répertoire où se trouvent les executable
+  system("./T1");			//Lancement de l'executable T1 lançant le TermSaver1
 }
 
-void execTerm2()
+void execTerm2()			//Fonction d'execution du TermSaver dynamique
 {
 	chdir(getenv("EXIASAVER_HOME"));
-	system("./T2");
+	system("./T2");	
 }
