788995838823261a4105e07676786ee9cb6f6a11
Mahkss <max.pavet@viacesi.fr>
2016-12-16 11:33:52 +0100

    Delete VE.c

--- Projet/test/VE.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(){
-
-//putenv("VERANDOM=VALEUR"); //int putenv("NAME=VALEUR")
-printf("ma ve est égale à: %s\n", getenv("VERANDOM"));
-return 0;
-}
